ace11d0a1e43d3fb840a8f59fd0731ff8fcbf5c3
Ruan <Ruan@Ruan>
2016-10-31 14:54:19 -0300

    Adicionei a função add do pet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -48,11 +48,39 @@ function ($scope, $stateParams, adicinarDono, $ionicPopup) {
 
 }])
 
-.controller('adicionarPetCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('adicionarPetCtrl', ['$scope', '$stateParams', 'PetClinic', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, adicionarPet, $ionicPopup) {
 
+
+    $scope.data = {
+           nomePet: '',
+           dataNascimento: '',
+           raca: ''
+
+       }
+
+       $scope.submitting = false;
+
+       $scope.submit = function(){
+           $scope.submitting = true;
+           adicionarPet.add($scope.data).then(function(){
+               $scope.data = {
+                 nomePet: '',
+                 dataNascimento: '',
+                 raca: ''
+
+               }
+               $scope.submitting = false;
+
+               $ionicPopup.alert({
+                   title: 'Thank you!',
+                   template: 'Your response has been recorded.'
+               });
+
+           })
+       }
 }])
 
 .controller('alterarPetCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
--- www/js/services2.js
+++ www/js/services2.js
@@ -4,13 +4,13 @@ angular.module('app.services', [])
 .service('PetClinic',['$http', function($http){
 
 
-  var api_url = 'https://sheetsu.com/apis/v1.0/251c7052222c';
+  var api_url2 = 'https://sheetsu.com/apis/v1.0/bdb44fb9f81c';
   var currentID = 1;
 
-  var ret = {
+  var ret2 = {
      all: function(){
 
-         return $http.get(api_url).then(function(resp){
+         return $http.get(api_url2).then(function(resp){
              if (resp.data.length > 0) currentID = parseInt(resp.data[resp.data.length-1].id);
              return resp.data;
          });
@@ -20,15 +20,15 @@ angular.module('app.services', [])
          currentID++;
          data.id = currentID;
 
-         return $http.post(api_url, data).then(function(resp){
+         return $http.post(api_url2, data).then(function(resp){
              return resp.data;
          });
 
      }
  }
 
-  ret.all();
+  ret2.all();
 
-  return ret;
+  return ret2;
 
 }]);
