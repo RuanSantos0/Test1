a26a7298117fa7bb710a52bc726b59e87108ff6f
Ruan <Ruan@Ruan>
2016-10-31 17:56:57 -0300

    Ajustes api

--- www/js/services.js
+++ www/js/services.js
@@ -3,31 +3,31 @@ angular.module('app.services', [])
 //Aqui é pra nomear o serviço. Esse abaixo será o Serviço Dono
 .service('DonoService',['$http', function($http){
 
-  var api_url = 'https://sheetsu.com/apis/v1.0/251c7052222c';
+  var api_url = 'https://sheetsu.com/apis/v1.0/8d92f5e7dd6e';
   var currentID = 1;
 
   var ret = {
         all: function(){
-            
+
             return $http.get(api_url).then(function(resp){
                 if (resp.data.length > 0) currentID = parseInt(resp.data[resp.data.length-1].id);
                 return resp.data;
             });
-            
-        }, 
+
+        },
         add: function(data){
             currentID++;
             data.id = currentID;
-            
+
             return $http.post(api_url, data).then(function(resp){
                 return resp.data;
             });
 
         }
     }
-     
+
      ret.all();
-     
+
      return ret;
 
 }])
@@ -63,14 +63,3 @@ angular.module('app.services', [])
     return ret2;
 
 }]);
-
-
-    
-
-
-
-
-
-
-
-
