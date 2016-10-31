5c25d5158c4074c9f678fd8a31f0bc29f9ff87e9
Ruan <Ruan@Ruan>
2016-10-31 15:03:09 -0300

    Ajustes

--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ angular.module('app.services', [])
 
 .service('PetClinic',['$http', function($http){
 
-
+console.log('test');
   var api_url = 'https://sheetsu.com/apis/v1.0/251c7052222c';
   var currentID = 1;
 
@@ -30,5 +30,33 @@ angular.module('app.services', [])
   ret.all();
 
   return ret;
+
+
+    var api_url2 = 'https://sheetsu.com/apis/v1.0/bdb44fb9f81c';
+    var currentID = 1;
+
+    var ret2 = {
+       all: function(){
+
+           return $http.get(api_url2).then(function(resp){
+               if (resp.data.length > 0) currentID = parseInt(resp.data[resp.data.length-1].id);
+               return resp.data;
+           });
+
+       },
+       add: function(data){
+           currentID++;
+           data.id = currentID;
+
+           return $http.post(api_url2, data).then(function(resp){
+               return resp.data;
+           });
+
+       }
+   }
+
+    ret2.all();
+
+    return ret2;
 
 }]);
